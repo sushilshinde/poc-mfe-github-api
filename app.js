94fde67f7112f3bc087acbe20d21fcded3d2f137
Aswathi Suseelan <aswathi.suseelan@AD.MSYSTECHNOLOGIES.COM>
2022-12-01 17:11:53 +0530

    added initial changes

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const reposList = require('./repos.json')
+
+const PORT = process.env.PORT || 9000;
+
+app.get('/repositories', (req, res) => {
+    res.json(reposList)
+})
+
+app.get('/commits', (req, res) => {
+    res.send('Commits')
+})
+
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`))
